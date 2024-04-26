85dec2ef11d940168ba857084fc4364776874798
ALVARO <rojasmachucaalvaro@gmail.com>
2024-04-26 20:36:27 -0300

    intentando subirlo

--- src/keys.js
+++ src/keys.js
@@ -1,13 +1,20 @@
 //dir de la db
+import {
+    DB_HOST,
+    DB_PORT,
+    DB_DATABASE,
+    DB_USER,
+    DB_PASSWORD
+} from './config.js'
 module.exports = {
 
     database: {
         connectionLimit: 1000,
-        host: 'roundhouse.proxy.rlwy.net',//'152.70.216.169',//'localhost', //'',
-        port: '14489',
-        user: 'root',//
-        password: 'BxUpAGGUzFEiVZnoYkIzhiBOFORShjDg',//'151199',
-        database: 'railway'
+        host: DB_HOST,//'152.70.216.169',//'localhost', //'',
+        port: DB_PORT,
+        user: DB_USER,//
+        password: DB_PASSWORD,//'151199',
+        database: DB_DATABASE
         // connectionLimit: 100,
         // host: 'localhost',//'152.70.216.169',//'localhost', //'',
         // port: '3307',
