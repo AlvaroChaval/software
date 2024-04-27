7c82a92f11945102b37c3bd9ac1c02be0c64b7f5
ALVARO <rojasmachucaalvaro@gmail.com>
2024-04-26 21:07:58 -0300

    arreglando el config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,18 @@
-import { config } from 'dotenv'
+const dotenv = require('dotenv');
+dotenv.config();
 
-config()
-export const PORT = process.env.PORT || 5000
-export const DB_USER= process.env.DB_USER || 'root'
-export const DB_PASSWORD= process.env.DB_PASSWORD || ''
-export const DB_HOST= process.env.DB_HOST || 'localhost'
-export const DB_DATABASE= process.env.DB_DATABASE || 'parcial'
-export const DB_PORT= process.env.DB_PORT || 3306
+const PORT = process.env.PORT || 5000;
+const DB_USER = process.env.DB_USER || 'root';
+const DB_PASSWORD = process.env.DB_PASSWORD || '';
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_DATABASE = process.env.DB_DATABASE || 'parcial';
+const DB_PORT = process.env.DB_PORT || 3306;
+
+module.exports = {
+    PORT,
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    DB_DATABASE,
+    DB_PORT
+  };
